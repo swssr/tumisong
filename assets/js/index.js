e3135f512f758cad157bebdf9b67af76d67acdd7
Brotnkr <21546142@dut4life.ac.za>
2018-08-28 00:55:25 +0200

    evething makes sense now

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,29 +3,38 @@
 let nav = document.querySelector(".nav--left");
 
 let col = document.querySelector(".col");
+let calEvents = document.querySelectorAll(".event");
+let clck = document.querySelectorAll(".click");
 let observer;
+let counter;
 
 var options = {
-  root: null,
+  root: nav,
   rootMargin: "0px",
   threshold: 1.0
 };
 
 observer = new IntersectionObserver(handleIntersect, options);
 
-function handleIntersect(entries) {
-  entries.forEach(entry => {
-    if (!entry.isIntersecting) {
-      // TweenMax.to(nav, 1, { left: -500 })
-      console.log(`hello its ${entry.isIntersecting}`);;
-      nav.classList.add('nav-up')
-    }
-    if (entry.isIntersecting) {
-      // TweenMax.to(nav, 1, { left: 0 });
-      console.log(`hello its ${entry.isIntersecting}`);
-      nav.classList.remove('nav-up')
-
-    }
-  });
-}
+function handleIntersect(entries) {}
 observer.observe(col);
+
+counter = 0;
+
+calEvents.forEach((calEvent, i) => {
+  calEvent.addEventListener('click', () => {
+    calEvents.forEach(c => {
+      c.classList.remove('active');
+    })
+    calEvents[i].classList.add('active')
+  })
+})
+
+clck.forEach((c, i) => {
+  c.addEventListener('click', _ => {
+    clck.forEach(cl => {
+      cl.classList.remove('active');
+    });
+    c.classList.add('active')
+  })
+})
