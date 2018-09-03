22066b88ce8b6118c66e5cc3f3f945ec6aaf445b
Brotnkr <21546142@dut4life.ac.za>
2018-09-03 10:48:53 +0200

    80 precent done

---
+++ server.js
@@ -0,0 +1,34 @@
+"use strict";
+//@ts-check
+
+let isPalendrom;
+let wordrev = "";
+
+let chkpalen = word => {
+  if (typeof wordrev === typeof word) {
+    wordrev = word
+      .split("")
+      .reverse()
+      .join("");
+    word == wordrev ? (isPalendrom = true) : (isPalendrom = false);
+    return isPalendrom == true
+      ? `${word} is a palendrom`
+      : `${word} is not palendrom`;
+  } else {
+    return `invalid input`;
+  }
+};
+console.log(chkpalen(""));
+
+let arr = Array(10);
+let value;
+
+function any40() {
+  arr.forEach(value => {
+    value = Math.floor(Math.random() * 1 * 40);
+    arr.push(value);
+  });
+  return arr;
+}
+
+console.log(any40());
