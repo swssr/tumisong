c3ed5da1fbbab023c0f2c1649368f2cdecb65b1e
Brotnkr <21546142@dut4life.ac.za>
2018-10-20 07:38:00 +0200

    all image observers working

--- assets/js/defer.js
+++ assets/js/defer.js
@@ -1,6 +1,12 @@
 //Lazy load images
+//Social grid images
 let feed__maker = document.querySelector('.feed__marker');
 let feed__imgs = document.querySelectorAll('.feed__item img');
+let c_art = document.querySelector('.cover-art img'),
+    v_tumb = document.querySelector('.thumbnail--video'),
+    last__bg = document.querySelector('.last__bg'),
+    b_art = document.querySelector('.form-img');
+let other__images = [c_art, v_tumb, b_art, last__bg];
 
 let feed_observer;
 
@@ -16,6 +22,7 @@ const renderImgs = (entries) => {
       feed__imgs.forEach(img => {
         img.src = img.dataset.src;
       });
+      feed_observer.unobserve(entry.target);
     }else{
       console.log(entry.isIntersecting);
     }
@@ -24,3 +31,24 @@ const renderImgs = (entries) => {
 feed_observer = new IntersectionObserver(renderImgs, img__options);
 
 feed_observer.observe(feed__maker);
+
+//Other below-fold images
+console.log(other__images);
+let other__observer;
+
+const observeOtherFN = (entries) => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      other__images.forEach(image => {
+        image.src = image.dataset.src;
+      });
+      other__observer.unobserve(entry.target)
+    }else{
+      return;
+    }
+  })
+}
+
+other__observer = new IntersectionObserver(observeOtherFN, img__options);
+
+other__images.forEach(img => other__observer.observe(img));
