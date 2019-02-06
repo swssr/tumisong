c4e01264db9d269003e47ee451d6c687b96b4e61
swssr <simocmo4@gmail.com>
2019-02-06 15:16:04 +0200

    updated fetch, api fix

--- docs/assets/js/playerwidget.js
+++ docs/assets/js/playerwidget.js
@@ -1,6 +1,6 @@
 //Music player widget
-let widgetArtist_tag = document.querySelector('.mp__artist-name');
-let widgetSongName_tag = document.querySelector('.mp__song-name');
+let widgetArtist_tag = document.querySelector('.mp__artist-name')
+let widgetSongName_tag = document.querySelector('.mp__song-name')
 let nowPlaying = {
   artist: null,
   song: null
@@ -8,8 +8,8 @@ let nowPlaying = {
 //Add source links and move to exp server
 const songitem_ul = document.querySelector('.mp__songs')
 const songitem_li = document.querySelectorAll('.mp__song')
-let playingSongName_tag = document.querySelector(`.playing .song__name`);
-let playingSongArtist_tag = document.querySelector(`.playing .song__artist`);
+let playingSongName_tag = document.querySelector(`.playing .song__name`)
+let playingSongArtist_tag = document.querySelector(`.playing .song__artist`)
 
 const populateMPList = (_maxcount) => {
   //Slice fetched songs to max of DOM link count
@@ -18,9 +18,9 @@ const populateMPList = (_maxcount) => {
       (`
       <li class="mp__song">
           <span class="mp__icon btn--play"></span>
-          <span class="song__name">${song.name}</span>
-          <span class="song__artist">${song.artist}</span>
-          <span class="song__duration">${song.duration}</span>
+          <span class="song__name">${song.song_name}</span>
+          <span class="song__artist">${song.artist_name}</span>
+          <span class="song__duration">0.00</span>
       </li>
       `)).join('')
 
