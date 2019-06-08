67b54379b07cdf0b6626536234abc7588d3ebe6a
swssr <simocmo4@gmail.com>
2019-06-08 03:04:18 +0200

    new event calender

--- docs/assets/js/index.js
+++ docs/assets/js/index.js
@@ -141,48 +141,36 @@ imgs_tag.forEach(img => IO.observe(img));
 const events = [
   {
     date: "12.10.17",
-    name: "Spring fiesta",
-    descr: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
-    odit voluptatum sunt asperiores!
-    Cum voluptatum ducimus recusandae quod ratione, culpa explicabo in corrupti nisi sit
-    optio
+    name: "Ultra South Africa",
+    img_url: "https://images.unsplash.com/photo-1505842465776-3b4953ca4f44?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+    descr: `Lorem ipsum
     eaque harum tenetur quos?`
   },
   {
     date: "12.10.17",
-    name: "Jazz fiesta",
-    descr: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
-    odit voluptatum sunt asperiores!
-    Cum voluptatum ducimus recusandae quod ratione, culpa explicabo in corrupti nisi sit
-    optio
-    eaque harum tenetur quos?`
+    name: "Cape Town International Jazz Festival",
+    img_url: 	"https://images.unsplash.com/13/unsplash_523b1f5aafc42_1.JPG?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=754&q=80",
+    descr: `Landae q?`
   },
   {
     date: "12.10.17",
-    name: "NYE explosive",
-    descr: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
-    odit voluptatum sunt asperiores!
-    Cum voluptatum ducimus recusandae quod ratione, culpa explicabo in corrupti nisi sit
-    optio
-    eaque harum tenetur quos?`
+    name: "AfrikaBurn",
+    img_url: 	"https://images.unsplash.com/photo-1557693116-fb9cff08f972?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80",
+    descr: `
+    Cum voluptatum duci quos?`
   },
   {
     date: "12.10.17",
-    name: "Dage whole thing",
-    descr: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
-    odit voluptatum sunt asperiores!
-    Cum voluptatum ducimus recusandae quod ratione, culpa explicabo in corrupti nisi sit
-    optio
-    eaque harum tenetur quos?`
+    name: "Vodacom Durban July",
+    img_url: 	"https://images.unsplash.com/photo-1526094798790-1df6f28275cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    descr: `Lorem ipsum dandae quod ratione, culpa explicabo in corruptur quos?`
   },
   {
     date: "12.10.17",
-    name: "Ke December",
+    name: "Umuzi Week of Festivals",
+    img_url: "https://images.unsplash.com/photo-1520483691742-bada60a1edd6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
     descr: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
-    odit voluptatum sunt asperiores!
-    Cum voluptatum ducimus recusandae quod ratione, culpa explicabo in corrupti nisi sit
-    optio
-    eaque harum tenetur quos?`
+    odit voluptatur quos?`
   }
 ];
 const DOMevents = document.querySelector(".events");
@@ -190,11 +178,15 @@ const popEvents = _events => {
   DOMevents.innerHTML = events
     .slice(0, 5)
     .map(event => {
+      //Should remove tab index find something better.
       return `  
     <li class="event">
+      <img class="event__bg" src="${event.img_url}" data-src="${event.img_url}" />
       <a href="#0"><h3 class="event__name">${event.name}</h3></a>
-      <p class="text text--light">${event.descr}</p>
-      <div class="event__date">${event.date}</div>
+      <div class="event__details">
+        <p class="paragraph text text--light">${event.descr}</p>
+        <div class="event__date text text--light">${event.date}</div>
+      </div>
     </li>
     `;
     })
@@ -204,6 +196,7 @@ const popEvents = _events => {
 popEvents();
 
 let calEvents = document.querySelectorAll(".event");
+let eventNames = document.querySelectorAll(".event__name");
 
 //Default active calender event
 calEvents[0].querySelector(".event__name").focus();
@@ -211,9 +204,9 @@ calEvents[0].querySelector(".event__name").focus();
 //Blur out overflowing events incrementally
 let value = 100;
 calEvents.forEach((v, index) => {
-  if(index > 1){
+  if (index > 1) {
     v.style["filter"] = `brightness(${value}%)`;
-    value -= 8;
+    value -= 14;
   }
 });
 //Onclick
@@ -222,11 +215,22 @@ calEvents.forEach((event, i) => {
     console.log("Clicking");
     makeActive(calEvents, event, "event--active");
   });
-  event.addEventListener("focus", () => {
-    console.log("Focusing");
-    makeActive(calEvents, event, "event--active");
-  });
 });
+
+//Event focus
+eventNames.forEach((v, i) => {
+  v.addEventListener("focus", () => {
+    console.log("focusing");
+    makeActive(eventNames, v, "event--active");
+  });
+  v.addEventListener("blur", () => {
+    console.log("blur");
+  });
+  console.log(v);
+});
+
+
+//Auto scroll
 
 //End Events
 
@@ -247,5 +251,5 @@ function makeActive(list, target = e.currentTarget, activeClass = "active") {
   list.forEach(item => {
     item.classList.remove(activeClass); //Reset on every click
   });
-  target.classList.add(activeClass);
+  target.classList.toggle(activeClass);
 }
